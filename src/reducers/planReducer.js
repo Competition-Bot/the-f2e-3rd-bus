b6d235718b97a5e1e9faa569684a1f66b3442a5c
Peiyun Lee <may1092200258@gmail.com>
2021-11-27 14:15:30 +0800

    feature - plandetail

--- src/reducers/planReducer.js
+++ src/reducers/planReducer.js
@@ -7,30 +7,32 @@ const initialState = {
   },
   planlist: [
     {
+      pos: ["桃園火車站", "桃園藝文廣場"],
       time: ["14:00", "14:23", "23"],
       step: [
-        { type: "walk", time: "5" },
+        { type: "walk", time: "5", destination:"統領百貨站"},
         {
           type: "bus",
           name: "152",
           start: "統領百貨",
           end: "大興西新埔六街口",
         },
-        { type: "walk", time: "7" },
+        { type: "walk", time: "7", destination:"桃園藝文廣場" },
       ],
       price: 18,
     },
     {
+      pos: ["桃園火車站", "桃園藝文廣場"],
       time: ["14:05", "14:26", "21"],
       step: [
-        { type: "walk", time: "5" },
+        { type: "walk", time: "5", destination:"統領百貨站" },
         {
           type: "bus",
           name: "151",
           start: "統領百貨",
           end: "中正藝文特區(福安宮)",
         },
-        { type: "walk", time: "5" },
+        { type: "walk", time: "5", destination:"桃園藝文廣場" },
       ],
       price: 18,
     },
